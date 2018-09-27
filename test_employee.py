8058ac58a92e03c84dbb2d3e214ebe583a2c52dd
Oleg Sliusar <olehsliusar@gmail.com>
2018-09-27 22:00:08 +0300

    Modify Employee test cases

--- test_employee.py
+++ test_employee.py
@@ -13,30 +13,30 @@ class TestEmployee(unittest.TestCase):
 
     def test_email(self):
         self.assertEqual(self.emp_1.email, 'Corey.Schafer@email.com')
-        self.assertEqual(emp_2.email, 'Sue.Smith@email.com')
+        self.assertEqual(self.emp_2.email, 'Sue.Smith@email.com')
 
         self.emp_1.first = 'John'
-        emp_2.first = 'Jane'
+        self.emp_2.first = 'Jane'
 
         self.assertEqual(self.emp_1.email, 'John.Schafer@email.com')
-        self.assertEqual(emp_2.email, 'Jane.Smith@email.com')
+        self.assertEqual(self.emp_2.email, 'Jane.Smith@email.com')
 
     def test_fullname(self):
         self.assertEqual(self.emp_1.fullname, 'Corey Schafer')
-        self.assertEqual(emp_2.fullname, 'Sue Smith')
+        self.assertEqual(self.emp_2.fullname, 'Sue Smith')
 
         self.emp_1.first = 'John'
-        emp_2.first = 'Jane'
+        self.emp_2.first = 'Jane'
 
         self.assertEqual(self.emp_1.fullname, 'John Schafer')
-        self.assertEqual(emp_2.fullname, 'Jane Smith')
+        self.assertEqual(self.emp_2.fullname, 'Jane Smith')
 
     def test_apply_raise(self):
         self.emp_1.apply_raise()
-        emp_2.apply_raise()
+        self.emp_2.apply_raise()
 
         self.assertEqual(self.emp_1.pay, 52500)
-        self.assertEqual(emp_2.pay, 63000)
+        self.assertEqual(self.emp_2.pay, 63000)
 
 
 if __name__ == '__main__':
